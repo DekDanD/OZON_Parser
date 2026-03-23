01885303ddb876d39c4112afda29ece913763610
Деканадзе Даниил Давидович <dekanadze2103@mail.ru>
2026-03-23 21:41:48 +0500

    ускорение подбора страниц и поиска sku

--- ТЗ_Деканадзе.py
+++ ТЗ_Деканадзе.py
@@ -1,11 +1,11 @@
 import json
 from datetime import datetime
 import time
-
+import re
 import camoufox
 
 config = {
-        "headless": True,    
+        "headless": False,    
         "os": "windows",     
         "geoip": True
     }
@@ -13,36 +13,31 @@ config = {
 def finding_something_in_ozon(query: str, sku_to_search: int):
     with camoufox.Camoufox(**config, humanize=True) as browser:
         products = set()
-        for i in range(1, 9):
-            page = browser.new_page()
-            page.goto(f"https://ozon.ru/search/?text=блейк крауч&page={i}&limit=100", wait_until="load")
-            while (True):
-                page.evaluate("document.body.style.zoom = '0.1'")
-                page.mouse.move(0, 1000)
-                page.wait_for_timeout(5000)
-                if (page.locator('div[data-widget="infiniteVirtualPaginator"] div[class^="tile-root"]').count() > 12):
-                        for j in page.locator('div[data-widget="infiniteVirtualPaginator"] div[class^="tile-root"]').all()[:12]:
-                            if j in products: continue
-                            sub = browser.new_page()
-                            href = j.locator('a').first.get_attribute('href')
-                            sub.goto(f"https://ozon.ru/{href[1:]}")
-                            sub.wait_for_timeout(2000)
-                            content = sub.content()
-                            start = content.index('Артикул: ') + 9
-                            end = start
-                            products.add(j)
-                            while content[end].isdigit():
-                                end += 1
-                            sku = content[start:end]
-                            if int(sku) == sku_to_search:
-                                return json.dumps({
-                                    "query": query,
-                                    "sku": sku,
-                                    "position": len(products),
-                                    "page": i,
-                                    "total_checked": 100,
-                                    "timestamp": datetime.now().isoformat()
-                                    })
-                            if len(products) == 100: break
-                        break
+        page = browser.new_page()
+        page.goto(f"https://ozon.ru/search/?text={query}&limit=100", wait_until="load")
+        page.wait_for_timeout(5000)
+        page.evaluate("document.body.style.zoom = '0.1'")
+        page.wait_for_timeout(20000)
+        print(page.locator('div[data-widget="infiniteVirtualPaginator"] div[class^="tile-root"]').count())
+        for j in page.locator('div[data-widget="infiniteVirtualPaginator"] div[class^="tile-root"]').all():
+            if j.text_content() in products: continue
+            products.add(j.text_content())
+            href = j.locator('a').first.get_attribute('href')
+            match = re.search(r'.*\/product\/.*\-([1-9][0-9]+)\/', href)
+            sku = 0
+            if match:
+                sku = match.group(1)
+            if int(sku) == sku_to_search:
+                return json.dumps({
+                    "query": query,
+                    "sku": sku,
+                    "position": len(products),
+                    "page": 1,
+                    "total_checked": 100,
+                    "timestamp": datetime.now().isoformat()
+                    })
+            if len(products) == 100: break
     return "not_found"
+
+if __name__ == "__main__":
+    finding_something_in_ozon('блейк крауч', 3260207120)
